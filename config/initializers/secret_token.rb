1a67305611965431e0c1f198ad41ed31489b1804
rodeofly <florian.tobe@gmail.com>
2013-09-19 13:46:57 +0400

    Remote commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sanddy::Application.config.secret_key_base = 'a80eee14fcd6d4b1362230f924132b05c18a85fb63c60f6ab634ef6e3439c58656096f2ed5c5b251fd16844e0002b93989f0cfe62f66f10e8146cf941b1438f9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sanddy::Application.config.session_store :cookie_store, key: '_sanddy_session'
