798f9cc7535decb0ab0a8cc8dcc040e67febaa3f
rodeofly <florian.tobe@gmail.com>
2013-09-20 09:44:17 +0400

    rails new . (Original rails 4 app)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sanddy::Application.config.secret_key_base = '0b19d09597fef60a8aaad19ebc81080208872b95f71213ae32bbeb340ecd69c45cd95b9fc07365bf815fa76a19c166c31fb79e158a817d4121d314d17f8a87b9'
